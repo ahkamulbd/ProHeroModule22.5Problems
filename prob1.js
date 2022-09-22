979e0085aeaea98a04ae3d498f97bc73232d7c5f
ahkamulbd <ahkamulbd@gmail.com>
2022-09-22 19:48:06 +0600

    Solutions of problems under Module 22.5

---
+++ prob1.js
@@ -0,0 +1,12 @@
+/*
+    1) Harry’s mom gave him tk 1000 and asked him to buy some oranges and
+    apples. Write a program to help Harry calculate how much money the
+    shopkeeper will return. The total cost of 1 kg of oranges and 1 kg of
+    apples is tk 700.
+*/
+
+let givenTaka = 1000;
+let costOfOrangesAndApples = 700;
+let returnTaka = givenTaka - costOfOrangesAndApples;
+
+console.log(returnTaka);
